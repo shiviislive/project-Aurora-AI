2eef38c2061b7ed68025470997420c3abb30d2ba
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-16 21:56:55 +0530

    Add socket authentication and message handling; update dependencies

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,13 +1,85 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie")
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
+const aiService = require("../services/ai.service")
+const messageModel = require("../models/message.model");
+
+
 
 function initSocketServer(httpServer) {
 
     const io = new Server(httpServer, {})
 
+    io.use(async (socket, next) => {
+
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+        if (!cookies.token) {
+            next(new Error("Authentication error: No token provided"));
+        }
+
+        try {
+
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+            const user = await userModel.findById(decoded.id);
+
+            socket.user = user
+
+            next()
+
+        } catch (err) {
+            next(new Error("Authentication error: Invalid token"));
+        }
+
+    })
 
     io.on("connection", (socket) => {
-        console.log("New socket connection:", socket.id);
+
+
+        socket.on("ai-message", async (messagePayload) => {
+
+
+
+
+            await messageModel.create({
+                chat: messagePayload.chat,
+                user: socket.user._id,
+                content: messagePayload.content,
+                role: "user"
+            })
+
+            const chatHistory = await messageModel.find({
+                chat: messagePayload.chat
+            })
+
+
+
+            const response = await aiService.generateResponse(chatHistory.map(item => {
+                return {
+                    role: item.role,
+                    parts: [ { text: item.content } ]
+                }
+            }))
+
+            await messageModel.create({
+                chat: messagePayload.chat,
+                user: socket.user._id,
+                content: response,
+                role: "model"
+            })
+
+            socket.emit('ai-response', {
+                content: response,
+                chat: messagePayload.chat
+            })
+
+        })
+
     })
+
+
 
 }
 
