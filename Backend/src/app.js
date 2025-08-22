1fd1c2c5ba2c9a486c970317d0437d62a48713c2
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-22 21:25:43 +0530

    initialize frontend with React, Vite, and essential routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 /* Routes */
 const authRoutes = require('./routes/auth.routes');
@@ -9,7 +11,12 @@ const chatRoutes = require("./routes/chat.routes");
 const app = express();
 
 
+
 /* using middlewares */
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
