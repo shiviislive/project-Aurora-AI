d9d0d5d0420b24a6f791101896d16bf5e6c40a30
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-08-23 22:43:56 +0530

    Add initial HTML structure, CSS styles, and SVG icon for the frontend
    - Created index.html with basic structure and linked CSS and JS files.
    - Added index-DEPrTWc_.css for styling the application with responsive design.
    - Included vite.svg as the favicon for the application.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 
 /* Routes */
@@ -19,11 +20,17 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 
 /* Using Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
+
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         setSubmitting(true);
         console.log(form);
 
-        axios.post("http://localhost:3000/api/auth/register", {
+        axios.post("https://cohort-1-project-chat-gpt.onrender.com/api/auth/register", {
             email: form.email,
             fullName: {
                 firstName: form.firstname,
